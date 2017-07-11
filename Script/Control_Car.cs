16c878217a9e829f820fe7b64f595b7de7d7c75d
Yoo JiSang <digndign2@naver.com>
2017-07-11 15:39:13 +0900

    싱글톤 추가 (게임 매니저) 아이템 클래스 생성

--- Script/Control_Car.cs
+++ Script/Control_Car.cs
@@ -11,17 +11,19 @@ public class Control_Car : MonoBehaviour {
     public float speedRotate = 100.0f;
 
     private float fTime = 0.0f;
+    string CollisionObjName = "";
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        GameManager.Instance.PlayTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
         Move_Speed();
         Move_Rotate();
         Update_Speed();
+        GameManager.Instance.PlayTime += Time.deltaTime;
 
     }
 
@@ -46,7 +48,7 @@ public class Control_Car : MonoBehaviour {
     void Update_Speed()
     {
         fTime += Time.deltaTime;
-        speedMove = 1.0f + (fTime * 3.0f);
+        speedMove = 1.0f + (fTime * 5.0f);
         if (speedMove > MaxSpeed)
             speedMove = MaxSpeed;
     }
@@ -55,26 +57,72 @@ public class Control_Car : MonoBehaviour {
     {
         fTime = 0.0f;
         speedMove = 1.0f;
+        if(collision.gameObject.tag != "MapObject")
+            CollisionObjName = "CollisionEnter " + collision.gameObject.name;
+        print(CollisionObjName);
     }
 
-    private void OnCollisionStay(Collision collision)
-    {
-    }
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if (collision.gameObject.tag != "MapObject")
+    //        CollisionObjName = "CollisionStay " + collision.gameObject.name;
+    //    print(CollisionObjName);
+    //}
 
     private void OnCollisionExit(Collision collision)
     {
+        if (collision.gameObject.tag != "MapObject")
+            CollisionObjName = "CollisionExit " + collision.gameObject.name;
+        print(CollisionObjName);
     }
 
     private void OnTriggerEnter(Collider col)
     {
+        if (col.gameObject.tag != "MapObject")
+            CollisionObjName = col.gameObject.name;
+        print("TriggerEnter" + CollisionObjName);
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-    }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag != "MapObject")
+    //        CollisionObjName = other.gameObject.name;
+    //}
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.gameObject.tag != "MapObject")
+            CollisionObjName = other.gameObject.name;
+    }
+
+    private void OnGUI()
+    {
+        // 같은 기능
+        GUI.TextArea(new Rect(10, 5, 400, 30), CollisionObjName);
+        GUI.TextField(new Rect(10, 45, 400, 30), CollisionObjName);
+        GUILayout.Label(CollisionObjName);  // << : 지정을 따로 해줘야함
+
+        // 버튼이 눌렸을때
+        if(GUI.Button(new Rect(100, 200, 200, 30), "아이템"))
+        {
+            GUI.TextField(new Rect(100, 200, 170, 30), "버튼 클릭");
+            print("버튼 클릭");
+        }
+
+        if (GUI.RepeatButton(new Rect(300, 300, 50, 30), "<<"))
+        {
+            float rotate = -1;
+            rotate = rotate * speedRotate * Time.deltaTime;
+            gameObject.transform.Rotate(Vector3.up * rotate);
+            print("좌회전");
+        }
+        if (GUI.RepeatButton(new Rect(400, 300, 50, 30), ">>"))
+        {
+            float rotate = +1;
+            rotate = rotate * speedRotate * Time.deltaTime;
+            gameObject.transform.Rotate(Vector3.up * rotate);
+            print("우회전");
+        }
     }
 
 }
---
+++ Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager sInstance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(sInstance == null)
+            {
+                GameObject newGameObject = new GameObject("_GameManager");
+                sInstance = newGameObject.AddComponent<GameManager>();
+            }
+            return sInstance;
+        }
+    }
+
+    /* 플레이어 정보
+     * 플레이 타임
+     */
+
+    public float PlayTime = 0.0f;
+
+    //스타트보다 먼저 실행되는 함수 (한번 호출이후 다시 호출되지는 않음)
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	void Update () {
+		
+	}
+}
